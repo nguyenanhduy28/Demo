4a7e8c18ef8a00f604c5747342bb7c82aa7014ab
Anh Duy <anhd280704@gmail.com>
2026-03-27 10:08:04 +0700

    first commit

---
+++ src/main/java/com/example/demo/InfoController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+
+    @GetMapping("/info")
+    public String info(){
+
+        String ten = "Nguyen Anh Duy";
+        String mssv = "2280600458";
+        String nhom = "Nhóm 3";
+
+        return "Tên: " + ten +
+               "<br>MSSV: " + mssv +
+               "<br>" + nhom;
+    }
+}
